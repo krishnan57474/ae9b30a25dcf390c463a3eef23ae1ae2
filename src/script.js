99028b8a284d9b9cc33e03d0c62b13c51b494837
Krishnan <krishnan57474@gmail.com>
2020-08-18 14:04:32 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,10 +1,89 @@
 (function () {
     "use strict";
 
-    function init() {
+    function messageAction(data) {
+        switch (data.action) {
+            case "dial": {
+                if (!data.number || (/[^0-9]/).test(data.number)) {
+                    break;
+                }
+
+                cordova.InAppBrowser.open(
+                    "tel:" + data.number,
+                    "_system"
+                );
+
+                navigator.app.exitApp();
+
+                break;
+            }
+
+            case "whatsapp": {
+                if (!data.number || (/[^0-9]/).test(data.number)) {
+                    break;
+                }
+
+                cordova.InAppBrowser.open(
+                    "whatsapp://send?phone=" + data.number,
+                    "_system"
+                );
+
+                navigator.app.exitApp();
+
+                break;
+            }
+        }
+    }
+
+    var Message = (function () {
+        function Message(iawin) {
+            var obj = this;
+
+            iawin.addEventListener("message", function (e) {
+                if (e.type !== "message" || !e.data || !e.data.action) {
+                    return;
+                }
+
+                obj.action(e.data);
+            });
+        }
+
+        Message.prototype = {
+            action: messageAction
+        };
+
+        return Message;
+    }());
+
+    function getAppUrl() {
         var url = atob("aHR0cHM6Ly9uZXdpbmRpYWJ1c2luZXNzLmluLw=="),
-        iawin = window.open(url, "_blank", "location=no,clearcache=yes,zoom=no,footer=no"),
+        previousUrl;
+
+        try {
+            previousUrl = localStorage.getItem("previousUrl");
+
+            if (previousUrl && previousUrl.indexOf(url) === 0) {
+                url = previousUrl;
+            }
+        } catch (e) {}
+
+        return url;
+    }
+
+    function init() {
+        var url = getAppUrl(),
+        iawin = cordova.InAppBrowser.open(url, "_blank", "location=no,clearcache=yes,zoom=no,footer=no"),
         error = false;
+
+        new Message(iawin);
+
+        iawin.addEventListener("loadstart", function (e) {
+            try {
+                if (e.url.indexOf("https") === 0) {
+                    localStorage.setItem("previousUrl", e.url);
+                }
+            } catch (e) {}
+        });
 
         iawin.addEventListener("loaderror", function () {
             error = true;
